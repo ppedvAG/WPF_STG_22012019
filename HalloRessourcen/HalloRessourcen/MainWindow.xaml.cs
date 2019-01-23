f95acc9f11e6fe6707425bb4992cf3facd9e5656
André <andrerrr@outlook.de>
2019-01-23 11:09:18 +0100

    Hallo ResourcessssAAAAAAAAaaaa

---
+++ HalloRessourcen/HalloRessourcen/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace HalloRessourcen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Resources["farbe"] = new SolidColorBrush(Colors.Aquamarine);
+        }
+    }
+}
